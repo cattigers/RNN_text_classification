760730603bf01b607260f6a87b7f4dd2f720b293
Vinh Nguyen <vinh.nguyenx@gmail.com>
2018-10-02 11:34:21 +1000

    revisit

--- start.sh
+++ start.sh
@@ -3,17 +3,21 @@
 MAX_DOC_LENGTH=20 
 python dbpedia_to_TFRecords.py --max_document_length ${MAX_DOC_LENGTH}
 
+gsutil cp char-train-${MAX_DOC_LENGTH}.tfrecords gs://vinh-tutorial/data/dbpedia/charRNN/${MAX_DOC_LENGTH}/
+gsutil cp char-test-${MAX_DOC_LENGTH}.tfrecords gs://vinh-tutorial/data/dbpedia/charRNN/${MAX_DOC_LENGTH}/
+
+
 python text_classification_TPU_withTFRecords.py \
- --use_tpu=False \
- --model_dir=.gs://vinh-tutorial/output/charRNN/${MAX_DOC_LENGTH}\
+ --use_tpu=True \
+ --model_dir=gs://vinh-tutorial/output/charRNN/${MAX_DOC_LENGTH}\
  --train_batch_size=1024 \
  --num_cores=8 \
  --learning_rate=0.1 \
- --data_dir=gs://vinh-tutorial/data/dbpedia/${MAX_DOC_LENGTH} \
+ --data_dir=gs://vinh-tutorial/data/dbpedia/charRNN/${MAX_DOC_LENGTH} \
  --rnn_size=128 \
  --max_document_length=${MAX_DOC_LENGTH} \
  --master=grpc://10.240.22.2:8470
-
+ 
 #GPU
 python3 text_classification_TPU_withTFRecords.py \
  --use_tpu=False \
